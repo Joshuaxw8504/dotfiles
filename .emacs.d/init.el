e86295111792c52524731c5a32e6d8671422fe4c
Joshua Wang <jw@archvm.localdomain>
2020-11-11 20:18:46 -0600

    added evil and evil-collection to emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -21,7 +21,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(vterm evil zenburn-theme dracula-theme exwm xelb use-package async)))
+   '(evil-collection vterm evil zenburn-theme dracula-theme exwm xelb use-package async)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
